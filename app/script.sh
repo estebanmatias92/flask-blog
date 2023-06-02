1b4b1a07749f5f0d0be55940c239028828248c39
Matias Esteban <estebanmatias92@gmail.com>
2023-06-02 23:59:02 +0000

    Remove activate_env function from script

--- app/script.sh
+++ app/script.sh
@@ -1,39 +1,15 @@
 #!/bin/sh
 
-# Set app service root directory
-#export APPDIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-# Set virtual environment path and venv/bin path
-VENVPATH="${APPDIR}/${VENV}"
-BINPATH="${VENVPATH}/bin"
-# Updating the PATH with project's-specific bin folders
-#export PATH=$HOME/.local/bin:$PATH
-
-# Automate the VENV path folder and environment creation
-create_env() {
-    echo "Creating virtual environment"
-    python3 -m venv "${VENVPATH}"
-}
-
-# Call the envrionment (For dev scripts only)
-activate_env() {
-    echo "Activating virtual envrionment"
-   . ${BINPATH}/activate
-}
+# Re-export the VIRTUAL_ENV var
+export VIRTUAL_ENV="${APPDIR}/venv"
 
 # Use entrypoint.sh to run the app with whatever command you need
 run() {
-    (activate_env; ${APPDIR}/entrypoint.sh)
+    (. $VIRTUAL_ENV/bin/activate; ${APPDIR}/entrypoint.sh)
 }
 
-# Aliases
-alias activate="activate_env"
-
 # Create the VENV if doesn't exists
-if [ ! -d "${VENVPATH}" ]; then
-    create_env
-fi
-
-# If the VENV has been created but it is not activated, activate it
-if  [ -d "${VENVPATH}" ]; then
-    activate_env
+if [ ! -d "${VIRTUAL_ENV}" ]; then
+    echo "Creating virtual environment"
+    python3 -m venv "${VIRTUAL_ENV}"
 fi
