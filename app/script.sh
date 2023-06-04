9848bb5d635e847ae6df2e57292f73dece3dfa86
Matias Esteban <estebanmatias92@gmail.com>
2023-06-04 00:55:58 +0000

    Adding conditional to update the PATH environment variable

--- app/script.sh
+++ app/script.sh
@@ -9,8 +9,11 @@ if [ ! -d "${VIRTUAL_ENV}" ]; then
     python3 -m venv "${VIRTUAL_ENV}"
 fi
 
-# Add the VENV bin folder to PATH
-export PATH="${VIRTUAL_ENV}/bin:${PATH}"
+# Add the venv binaries to the PATH
+if ! echo $PATH | grep -q "${VIRTUAL_ENV}/bin:"; then
+    echo "Adding venv bin to PATH"
+    export PATH="${VIRTUAL_ENV}/bin:${PATH}"
+fi
 
 # Use entrypoint.sh to run the app with whatever command you need
 run() {
